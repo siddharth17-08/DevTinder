ec005c5b59bb7cf9eaedba0e9012a1568d3f7ec7
sohamnagare17 <nagaresoham7@gmail.com>
2025-04-08 01:23:37 +0530

    Frontend Updated

--- frontend/src/components/Common/Footer.jsx
+++ frontend/src/components/Common/Footer.jsx
@@ -2,7 +2,7 @@ const Footer = () => {
     return (
         <footer className="footer sm:footer-horizontal footer-center bg-black/30 text-base-content p-6 mt-auto">
             <aside>
-                <p>Copyright © 2025 - All right reserved by Sharadindu Das</p>
+                <p>Copyright © 2025 - All right reserved by Siddharth Kumbhar</p>
             </aside>
         </footer>
     );
